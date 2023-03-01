3f281392c2776ba2d6cc75cb450599e0739320e0
Łukasz <lukaszkogut1996@gmail.com>
2023-03-01 21:53:06 +0100

    +

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery')
+
+const imagesHTML = galleryItems.map(({ preview, original, description }) => {
+	return `<div class='gallery__item'>
+	  <a class='gallery__link' href='${original}'>
+		<img class='gallery__image' src='${preview}' data-source='${original}' alt='${description}' />
+	  </a>
+	</div>`;
+  });
+  gallery.insertAdjacentHTML('beforeend', imagesHTML.join(''));
+
+  gallery.addEventListener('click', event => {
+	event.preventDefault()
+	if (event.target.tagName === 'IMG') {
+		const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}" alt="${event.target.alt}">`,
+	  {
+		onClose: instance => {gallery.removeEventListener('keydown', onEscapePress);},
+	  }
+    );
+
+    const onEscapePress = (event) => {
+      if (event.key === 'Escape') {
+			  instance.close();
+			}
+    };
+		instance.show();
+		gallery.addEventListener('keydown', onEscapePress);
+	};
+});
